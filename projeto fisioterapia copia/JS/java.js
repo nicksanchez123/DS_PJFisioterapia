e3e5bd967d8662bcb1eb58e630c0b69cedc1d25a
nicksanchez123 <167665718+nicksanchez123@users.noreply.github.com>
2024-09-15 03:42:40 -0300

    Add files via upload

---
+++ projeto fisioterapia copia/JS/java.js
@@ -0,0 +1,18 @@
+function MaisSobre() {
+    var pontos = document.getElementById("pontos");
+    var maisTexto = document.getElementById("mais");
+    var btnLermais = document.getElementById("btnLerMais");
+
+    if (pontos.style.display === "none") {
+        pontos.style.display = "inline";
+        maisTexto.style.display = "none";
+        btnLermais.innerHTML = "Mais Sobre";
+    } else {
+        pontos.style.display = "none";
+        maisTexto.style.display = "inline";
+        btnLermais.innerHTML = "Fechar";
+    }
+
+}
+
+//funções para a parte de slideshow
