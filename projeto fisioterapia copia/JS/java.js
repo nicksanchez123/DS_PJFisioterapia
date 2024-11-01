6b16ca50d4dff353f83879cdff848813f8776393
unknown <felipesilvaam626@gmail.com>
2024-10-31 21:29:49 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaa

--- projeto fisioterapia copia/JS/java.js
+++ projeto fisioterapia copia/JS/java.js
@@ -20,4 +20,19 @@ $(document).on('hidden.bs.modal',function(event){
      $('body').addClass('modal-open')
     }
    })
+const x = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
+
    
